575592749507119ed60ad0c7c474d725a9ccba62
Kyle Euman <euman_kyle@yahoo.com>
2021-06-15 19:31:26 -0400

    changes to app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import singlePost from './Pages/singlePost';
+import Post from './Pages/Post';
+import Project from './Pages/Project';
+import NavBar from './Pages/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <NavBar />
+        <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/About' />
+        <Route component={singlePost} path='/post/:slug' />
+        <Route component={Post} path='/Post' />
+        <Route component={Project} path='/Project' />
+        </Switch>
+        </BrowserRouter>
   );
 }
 
