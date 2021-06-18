69d1ea834c02e7c6b1420261405d716ab2d0a45d
Kyle Euman <euman_kyle@yahoo.com>
2021-06-18 13:48:53 -0400

    herkou build

--- portfolio/src/component/Home.js
+++ portfolio/src/component/Home.js
@@ -1,18 +1,16 @@
 import  React from 'react';
 import image from '../Assets/images/bluebrimage.jpg'
-import Footer from './Footer'
+
 
 export default function Home() {
     return (
-        <>
-        <Footer/>
         <main>
             <img src={image} alt="blue backround" className='absolute object-cover w-full h-full'/>
             <section className='relative flex justify center min-h-screen pt-12 lg:pt-64 px-8'>
         <h1 className='text-6xl text-white font-bold leading-none lg: leading-snug '>Hello I am Kyle Euman</h1>
         </section>
         </main>
-       </>
+       
     )
     
     
