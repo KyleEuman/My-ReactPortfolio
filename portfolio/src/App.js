df9c18485562f216a976ba3b857e647388c4f6fe
Kyle Euman <euman_kyle@yahoo.com>
2021-06-19 10:39:04 -0400

    heroku issues

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Switch} from "react-router-dom";
-import Home from './component/Home.js';
-import About from './component/About.js';
-import singlePost from './component/singlePost.js';
-import Post from './component/Post.js';
-import Project from './component/Project.js';
-import NavBar from './component/NavBar.js';
+import Home from './component/Home';
+import About from './component/About.';
+import singlePost from './component/singlePost.';
+import Post from './component/Post.';
+import Project from './component/Project.';
+import NavBar from './component/NavBar.';
 
 function App() {
   return (
